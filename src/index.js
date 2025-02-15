d2b8e553ed1c0fd2efe1e6d2af69f7110e050121
quarterback <contact@ronbronson.com>
2025-02-14 20:39:46 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Optional: Include your global styles
 import App from './App';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './components/ui/dialog';
 
 ReactDOM.render(
   <React.StrictMode>
