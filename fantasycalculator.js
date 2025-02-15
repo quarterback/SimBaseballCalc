5f09455cf43a58e3ee2f69e63d88b1e9a9c35a52
quarterback <contact@ronbronson.com>
2025-02-14 19:30:46 -0800

    Create fantasycalculator.js

---
+++ fantasycalculator.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import Papa from 'papaparse';
+import _ from 'lodash';
+
+const FantasyCalculator = () => {
+    // Your full FantasyCalculator component code
+};
+
+export default FantasyCalculator;
