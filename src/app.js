9ee603f0df70b189eafa68b860cb6a436bc749f5
quarterback <contact@ronbronson.com>
2025-02-14 19:31:54 -0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FantasyCalculator from './FantasyCalculator';
+
+function App() {
+    return (
+        <div className="App">
+            <FantasyCalculator />
+        </div>
+    );
+}
+
+export default App;
