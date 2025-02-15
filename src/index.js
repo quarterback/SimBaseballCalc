d392dff49b4edd57e72b8df886ca0368ee330dad
quarterback <contact@ronbronson.com>
2025-02-14 21:08:04 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Optional: Include your global styles
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
