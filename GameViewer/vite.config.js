02bef88eb48475aa9417cdaa22cbd085b1ae7ffe
quarterback <contact@ronbronson.com>
2025-03-24 23:27:41 -0700

    Update vite.config.js

--- GameViewer/vite.config.js
+++ GameViewer/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  base: '/GameViewer/', // 👈 this tells Vite to build for subpath deployment
+  base: '/GameViewer/', // important for Netlify subpath
   plugins: [svelte()],
   build: {
     outDir: 'dist',
